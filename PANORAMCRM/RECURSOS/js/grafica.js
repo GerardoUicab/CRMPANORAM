643d1688d0b469b2662e8476a88f2901f98b75c6
Gerardo Uicab <16090630@alumno.utmetropolitana.edu.mx>
2019-05-20 14:19:12 -0500

    cambios

---
+++ PANORAMCRM/RECURSOS/js/grafica.js
@@ -0,0 +1,15 @@
+﻿var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+        labels: ["Reparación", "Mantenimiento", "Asesorias", "Istalación"],
+        datasets: [{
+            data: [300, 50, 100, 40],
+            backgroundColor: ["#0099e5", "#46BFBD", "#FDB45C", "#949FB1"],
+            hoverBackgroundColor: ["#00bce4", "#5AD3D1", "#FFC870", "#A8B3C5"]
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
